4a704397ca717b23c9958f9efb99f6ff69e09a86
Andrea Nardo <andrea24695@gmail.com>
2023-03-28 11:21:24 +0200

    pre-childer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { DetailComponent } from './components/recipes/detail/detail.component';
 
 const routes: Routes = [
 {path: '', redirectTo: 'home', pathMatch: 'full'},
 {path: 'home', component: HomeComponent},
 {path: 'ricette', component: RecipesComponent},
+{path: 'dettaglio/:_id', component: DetailComponent},
 {path: '**', redirectTo: 'home'}
 ];
 
